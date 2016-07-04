fa0114d96042603d88ec0dd9bacbaff58851245a
Rk <rkramesh@users.noreply.github.com>
2016-07-04 17:13:31 +0530

    initial Commit

---
+++ imgdownloader.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import bs4 
+import re
+import os
+import requests
+import urllib2
+def search(query):
+    DIR=r"#Enter the dircetory name to save the images"
+    img_name = query
+    counter = 0
+    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+    
+
+    header = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url,
+                                headers={'User-agent': 'Mozilla/5.0 (Windows NT '
+                                                       '6.2; WOW64) AppleWebKit/'
+                                                       '537.36 (KHTML, like '
+                                                       'Gecko) Chrome/37.0.2062.'
+                                                       '120 Safari/537.36'})
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+    images=[tag['src']for tag in soup.find_all('img', {'src': re.compile('gstatic.com')})]
+    if images:
+        for pic in images:
+            raw_img = urllib2.urlopen(pic).read()
+            
+            counter = len([i for i in os.listdir(DIR) if img_name in i]) + 1
+            fdata = open(DIR + img_name + "_"+ str(counter)+".jpg", 'wb')
+            print str(counter) +' images found for '+query+'...'
+            fdata.write(raw_img)
+            fdata.close()
+    else:
+        print 'No Images Found'
+        
+    print 'Total '+ str(counter) +' images downloaded in '+DIR
+
+if __name__ == "__main__":
+    query = input("Enter yousearch for?  eg: 'Dolphin' ")
+    product_list = search(query)
+        
+
+
+  
+     
